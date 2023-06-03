521aa4cafc324e29c2c6ba32b6bf618a368a6392
KyleKotowick <kotowick@invictonlabs.com>
2023-06-03 16:48:33 -0400

    Bump TF and module versions; utilize nullable variable parameter

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 module "bucket_a" {
   source  = "Invicton-Labs/secure-s3-bucket/aws"
-  version = "~>0.2.2"
+  version = "~>0.3.0"
   providers = {
     aws = aws.a
   }
@@ -10,22 +10,22 @@ module "bucket_a" {
   // Versioning always required for replication
   versioned                     = true
   name                          = var.name_a
-  mfa_delete_enabled            = local.mfa_delete_enabled_a
-  bucket_policy_json_documents  = concat(local.bucket_policy_json_documents_a, local.bucket_policy_json_documents_both)
+  mfa_delete_enabled            = var.mfa_delete_enabled_a
+  bucket_policy_json_documents  = concat(var.bucket_policy_json_documents_a, var.bucket_policy_json_documents_both)
   kms_key_arn                   = var.kms_key_arn_a
-  create_new_kms_key            = local.create_new_kms_key_a
-  create_replica_kms_key        = local.create_replica_kms_key_a
-  kms_key_policy_json_documents = concat(local.kms_key_policy_json_documents_a, local.kms_key_policy_json_documents_both)
-  enable_transfer_acceleration  = local.enable_transfer_acceleration_a
-  block_public_acls             = local.block_public_acls_a
-  block_public_policy           = local.block_public_policy_a
-  ignore_public_acls            = local.ignore_public_acls_a
-  restrict_public_buckets       = local.restrict_public_buckets_a
-  object_ownership              = local.object_ownership_a
-  append_region_suffix          = local.append_region_suffix_a
-  object_lock_enabled           = local.object_lock_enabled_a
-  force_destroy                 = local.force_destroy_a
-  force_allow_cloudtrail_digest = local.force_allow_cloudtrail_digest_a
+  create_new_kms_key            = var.create_new_kms_key_a
+  create_replica_kms_key        = var.create_replica_kms_key_a
+  kms_key_policy_json_documents = concat(var.kms_key_policy_json_documents_a, var.kms_key_policy_json_documents_both)
+  enable_transfer_acceleration  = var.enable_transfer_acceleration_a
+  block_public_acls             = var.block_public_acls_a
+  block_public_policy           = var.block_public_policy_a
+  ignore_public_acls            = var.ignore_public_acls_a
+  restrict_public_buckets       = var.restrict_public_buckets_a
+  object_ownership              = var.object_ownership_a
+  append_region_suffix          = var.append_region_suffix_a
+  object_lock_enabled           = var.object_lock_enabled_a
+  force_destroy                 = var.force_destroy_a
+  force_allow_cloudtrail_digest = var.force_allow_cloudtrail_digest_a
 }
 
 locals {
@@ -34,9 +34,9 @@ locals {
   b_kms_key_arn = var.kms_key_arn_b != null ? var.kms_key_arn_b : module.bucket_a.kms_key_arn
   // If a specific key was provided for bucket B, or we're supposed to create a new key for bucket B,
   // use the input `create_replica_kms_key_b` variable as-is. 
-  b_create_replica_key = var.kms_key_arn_b != null || local.create_new_kms_key_b ? local.create_replica_kms_key_b : (
+  b_create_replica_key = var.kms_key_arn_b != null || var.create_new_kms_key_b ? var.create_replica_kms_key_b : (
     // Otherwise, check if a key was used for Bucket A (one provided, or new one created)
-    var.kms_key_arn_a != null || local.create_new_kms_key_a ? (
+    var.kms_key_arn_a != null || var.create_new_kms_key_a ? (
       // If a key was used for bucket A, only create a replica for bucket B if B is in a different region
       data.aws_region.a.name != data.aws_region.b.name
       ) : (
@@ -48,7 +48,7 @@ locals {
 
 module "bucket_b" {
   source  = "Invicton-Labs/secure-s3-bucket/aws"
-  version = "~>0.2.2"
+  version = "~>0.3.0"
   providers = {
     aws = aws.b
   }
@@ -58,27 +58,27 @@ module "bucket_b" {
   // Versioning always required for replication
   versioned                     = true
   name                          = var.name_b
-  mfa_delete_enabled            = local.mfa_delete_enabled_b
-  bucket_policy_json_documents  = concat(local.bucket_policy_json_documents_b, local.bucket_policy_json_documents_both)
+  mfa_delete_enabled            = local.var_mfa_delete_enabled_b
+  bucket_policy_json_documents  = concat(var.bucket_policy_json_documents_b, var.bucket_policy_json_documents_both)
   kms_key_arn                   = local.b_kms_key_arn
-  create_new_kms_key            = local.create_new_kms_key_b
+  create_new_kms_key            = var.create_new_kms_key_b
   create_replica_kms_key        = local.b_create_replica_key
-  kms_key_policy_json_documents = concat(local.kms_key_policy_json_documents_b, local.kms_key_policy_json_documents_both)
-  enable_transfer_acceleration  = local.enable_transfer_acceleration_b
-  block_public_acls             = local.block_public_acls_b
-  block_public_policy           = local.block_public_policy_b
-  ignore_public_acls            = local.ignore_public_acls_b
-  restrict_public_buckets       = local.restrict_public_buckets_b
-  object_ownership              = local.object_ownership_b
-  append_region_suffix          = local.append_region_suffix_b
-  object_lock_enabled           = local.object_lock_enabled_b
-  force_destroy                 = local.force_destroy_b
-  force_allow_cloudtrail_digest = local.force_allow_cloudtrail_digest_b
+  kms_key_policy_json_documents = concat(var.kms_key_policy_json_documents_b, var.kms_key_policy_json_documents_both)
+  enable_transfer_acceleration  = local.var_enable_transfer_acceleration_b
+  block_public_acls             = local.var_block_public_acls_b
+  block_public_policy           = local.var_block_public_policy_b
+  ignore_public_acls            = local.var_ignore_public_acls_b
+  restrict_public_buckets       = local.var_restrict_public_buckets_b
+  object_ownership              = local.var_object_ownership_b
+  append_region_suffix          = local.var_append_region_suffix_b
+  object_lock_enabled           = local.var_object_lock_enabled_b
+  force_destroy                 = local.var_force_destroy_b
+  force_allow_cloudtrail_digest = local.var_force_allow_cloudtrail_digest_b
 }
 
 module "replication" {
   source  = "Invicton-Labs/secure-s3-bucket-replication/aws"
-  version = "~>0.1.0"
+  version = "~>0.2.0"
   providers = {
     aws.a = aws.a
     aws.b = aws.b
@@ -87,6 +87,6 @@ module "replication" {
   bucket_b_module  = module.bucket_b
   replicate_a_to_b = var.replicate_a_to_b
   replicate_b_to_a = var.replicate_b_to_a
-  a_to_b_rules     = local.a_to_b_rules
-  b_to_a_rules     = local.b_to_a_rules
+  a_to_b_rules     = var.a_to_b_rules
+  b_to_a_rules     = var.b_to_a_rules
 }
--- variables.tf
+++ variables.tf
@@ -1,28 +1,20 @@
 variable "name_a" {
   description = "The name to use for bucket A."
   type        = string
-  validation {
-    condition     = var.name_a != null
-    error_message = "`name_a` may not be `null`."
-  }
+  nullable    = false
 }
 
 variable "name_b" {
   description = "The name to use for bucket B."
   type        = string
-  validation {
-    condition     = var.name_b != null
-    error_message = "`name_b` may not be `null`."
-  }
+  nullable    = false
 }
 
 variable "mfa_delete_enabled_a" {
   description = "Whether MFA Delete should be enabled for the bucket A."
   type        = bool
   default     = false
-}
-locals {
-  mfa_delete_enabled_a = var.mfa_delete_enabled_a != null ? var.mfa_delete_enabled_a : false
+  nullable    = false
 }
 variable "mfa_delete_enabled_b" {
   description = "Whether MFA Delete should be enabled for the bucket B. If not provided, will default to the value of `mfa_delete_enabled_a`."
@@ -30,34 +22,28 @@ variable "mfa_delete_enabled_b" {
   default     = null
 }
 locals {
-  mfa_delete_enabled_b = var.mfa_delete_enabled_b != null ? var.mfa_delete_enabled_b : var.mfa_delete_enabled_a
+  var_mfa_delete_enabled_b = var.mfa_delete_enabled_b != null ? var.mfa_delete_enabled_b : var.mfa_delete_enabled_a
 }
 
 variable "bucket_policy_json_documents_a" {
   description = "A list of JSON-encoded policy documents to apply to bucket A. The placeholder \"{BUCKET_ARN}\" can be used to reference the ARN of the bucket the policy is being applied to."
   type        = list(string)
   default     = []
-}
-locals {
-  bucket_policy_json_documents_a = var.bucket_policy_json_documents_a != null ? var.bucket_policy_json_documents_a : []
+  nullable    = false
 }
 
 variable "bucket_policy_json_documents_b" {
   description = "A list of JSON-encoded policy documents to apply to bucket B. The placeholder \"{BUCKET_ARN}\" can be used to reference the ARN of the bucket the policy is being applied to."
   type        = list(string)
   default     = []
-}
-locals {
-  bucket_policy_json_documents_b = var.bucket_policy_json_documents_b != null ? var.bucket_policy_json_documents_b : []
+  nullable    = false
 }
 
 variable "bucket_policy_json_documents_both" {
   description = "A list of JSON-encoded policy documents to apply to both buckets. The placeholder \"{BUCKET_ARN}\" can be used to reference the ARN of the bucket the policy is being applied to."
   type        = list(string)
   default     = []
-}
-locals {
-  bucket_policy_json_documents_both = var.bucket_policy_json_documents_both != null ? var.bucket_policy_json_documents_both : []
+  nullable    = false
 }
 
 variable "kms_key_arn_a" {
@@ -76,71 +62,55 @@ variable "create_new_kms_key_a" {
   description = "Whether to create a new KMS key for use with bucket A. If `kms_key_arn_a` is not provided (null), a new key will be created. If `kms_key_arn_a` is provided, a replica of that key will be created."
   type        = bool
   default     = false
-}
-locals {
-  create_new_kms_key_a = var.create_new_kms_key_a != null ? var.create_new_kms_key_a : false
+  nullable    = false
 }
 
 variable "create_new_kms_key_b" {
   description = "Whether to create a new KMS key for use with bucket B. If `kms_key_arn_b` is not provided (null), a new key will be created. If `kms_key_arn_b` is provided, a replica of that key will be created."
   type        = bool
   default     = false
-}
-locals {
-  create_new_kms_key_b = var.create_new_kms_key_b != null ? var.create_new_kms_key_b : false
+  nullable    = false
 }
 
 variable "create_replica_kms_key_a" {
   description = "Whether to create a replica of the `kms_key_arn_a` key for use with bucket A, if `kms_key_arn_a` was provided."
   type        = bool
   default     = false
-}
-locals {
-  create_replica_kms_key_a = var.create_replica_kms_key_a != null ? var.create_replica_kms_key_a : false
+  nullable    = false
 }
 variable "create_replica_kms_key_b" {
   description = "Whether to create a replica of the `kms_key_arn_b` key for use with bucket B, if `kms_key_arn_b` was provided."
   type        = bool
   default     = false
-}
-locals {
-  create_replica_kms_key_b = var.create_replica_kms_key_b != null ? var.create_replica_kms_key_b : false
+  nullable    = false
 }
 
 variable "kms_key_policy_json_documents_a" {
   description = "A list of JSON-encoded policy documents to apply to the KMS key created for bucket A, if one should be created."
   type        = list(string)
   default     = []
-}
-locals {
-  kms_key_policy_json_documents_a = var.kms_key_policy_json_documents_a != null ? var.kms_key_policy_json_documents_a : []
+  nullable    = false
 }
 
 variable "kms_key_policy_json_documents_b" {
   description = "A list of JSON-encoded policy documents to apply to the KMS key created for bucket B, if one should be created."
   type        = list(string)
   default     = []
-}
-locals {
-  kms_key_policy_json_documents_b = var.kms_key_policy_json_documents_b != null ? var.kms_key_policy_json_documents_b : []
+  nullable    = false
 }
 
 variable "kms_key_policy_json_documents_both" {
   description = "A list of JSON-encoded policy documents to apply to the KMS keys created for each bucket, if a KMS key was created."
   type        = list(string)
   default     = []
-}
-locals {
-  kms_key_policy_json_documents_both = var.kms_key_policy_json_documents_both != null ? var.kms_key_policy_json_documents_both : []
+  nullable    = false
 }
 
 variable "enable_transfer_acceleration_a" {
   description = "Whether to enable transfer acceleration for bucket A."
   type        = bool
   default     = false
-}
-locals {
-  enable_transfer_acceleration_a = var.enable_transfer_acceleration_a != null ? var.enable_transfer_acceleration_a : false
+  nullable    = false
 }
 variable "enable_transfer_acceleration_b" {
   description = "Whether to enable transfer acceleration for bucket B. If not provided, will default to the value of `enable_transfer_acceleration_a`."
@@ -148,16 +118,14 @@ variable "enable_transfer_acceleration_b" {
   default     = null
 }
 locals {
-  enable_transfer_acceleration_b = var.enable_transfer_acceleration_b != null ? var.enable_transfer_acceleration_b : local.enable_transfer_acceleration_a
+  var_enable_transfer_acceleration_b = var.enable_transfer_acceleration_b != null ? var.enable_transfer_acceleration_b : var.enable_transfer_acceleration_a
 }
 
 variable "block_public_acls_a" {
   description = "The `block_public_acls` value of an `aws_s3_bucket_public_access_block` resource that is applied to bucket A."
   type        = bool
   default     = true
-}
-locals {
-  block_public_acls_a = var.block_public_acls_a != null ? var.block_public_acls_a : true
+  nullable    = false
 }
 variable "block_public_acls_b" {
   description = "The `block_public_acls` value of an `aws_s3_bucket_public_access_block` resource that is applied to bucket B. If not provided, will default to the value of `enable_transfer_acceleration_a`."
@@ -165,16 +133,14 @@ variable "block_public_acls_b" {
   default     = null
 }
 locals {
-  block_public_acls_b = var.block_public_acls_b != null ? var.block_public_acls_b : local.block_public_acls_a
+  var_block_public_acls_b = var.block_public_acls_b != null ? var.block_public_acls_b : var.block_public_acls_a
 }
 
 variable "block_public_policy_a" {
   description = "The `block_public_policy` value of an `aws_s3_bucket_public_access_block` resource that is applied to bucket A."
   type        = bool
   default     = true
-}
-locals {
-  block_public_policy_a = var.block_public_policy_a != null ? var.block_public_policy_a : true
+  nullable    = false
 }
 variable "block_public_policy_b" {
   description = "The `block_public_policy` value of an `aws_s3_bucket_public_access_block` resource that is applied to bucket B. If not provided, will default to the value of `block_public_policy_a`."
@@ -182,16 +148,14 @@ variable "block_public_policy_b" {
   default     = null
 }
 locals {
-  block_public_policy_b = var.block_public_policy_b != null ? var.block_public_policy_b : local.block_public_policy_a
+  var_block_public_policy_b = var.block_public_policy_b != null ? var.block_public_policy_b : var.block_public_policy_a
 }
 
 variable "ignore_public_acls_a" {
   description = "The `ignore_public_acls` value of an `aws_s3_bucket_public_access_block` resource that is applied to bucket A."
   type        = bool
   default     = true
-}
-locals {
-  ignore_public_acls_a = var.ignore_public_acls_a != null ? var.ignore_public_acls_a : true
+  nullable    = false
 }
 variable "ignore_public_acls_b" {
   description = "The `ignore_public_acls` value of an `aws_s3_bucket_public_access_block` resource that is applied to bucket B. If not provided, will default to the value of `block_public_policy_a`."
@@ -199,16 +163,14 @@ variable "ignore_public_acls_b" {
   default     = null
 }
 locals {
-  ignore_public_acls_b = var.ignore_public_acls_b != null ? var.ignore_public_acls_b : local.ignore_public_acls_a
+  var_ignore_public_acls_b = var.ignore_public_acls_b != null ? var.ignore_public_acls_b : var.ignore_public_acls_a
 }
 
 variable "restrict_public_buckets_a" {
   description = "The `restrict_public_buckets` value of an `aws_s3_bucket_public_access_block` resource that is applied to bucket A."
   type        = bool
   default     = true
-}
-locals {
-  restrict_public_buckets_a = var.restrict_public_buckets_a != null ? var.restrict_public_buckets_a : true
+  nullable    = false
 }
 variable "restrict_public_buckets_b" {
   description = "The `restrict_public_buckets` value of an `aws_s3_bucket_public_access_block` resource that is applied to bucket B. If not provided, will default to the value of `restrict_public_buckets_a`."
@@ -216,16 +178,14 @@ variable "restrict_public_buckets_b" {
   default     = null
 }
 locals {
-  restrict_public_buckets_b = var.restrict_public_buckets_b != null ? var.restrict_public_buckets_b : local.restrict_public_buckets_a
+  var_restrict_public_buckets_b = var.restrict_public_buckets_b != null ? var.restrict_public_buckets_b : var.restrict_public_buckets_a
 }
 
 variable "object_ownership_a" {
   description = "The `rule.object_ownership` value of an `aws_s3_bucket_ownership_controls` resource that is applied to bucket A."
   type        = string
   default     = "BucketOwnerEnforced"
-}
-locals {
-  object_ownership_a = var.object_ownership_a != null ? var.object_ownership_a : "BucketOwnerEnforced"
+  nullable    = false
 }
 variable "object_ownership_b" {
   description = "The `rule.object_ownership` value of an `aws_s3_bucket_ownership_controls` resource that is applied to bucket B. If not provided, will default to the value of `object_ownership_a`."
@@ -233,16 +193,14 @@ variable "object_ownership_b" {
   default     = null
 }
 locals {
-  object_ownership_b = var.object_ownership_b != null ? var.object_ownership_b : local.object_ownership_a
+  var_object_ownership_b = var.object_ownership_b != null ? var.object_ownership_b : var.object_ownership_a
 }
 
 variable "append_region_suffix_a" {
   description = "If `true`, a suffix in the form of `-{region_name}` will be appended to the name of bucket A. This is convenient if you're creating buckets in multiple regions and don't want to manually specify the region name in each one for uniqueness."
   type        = bool
   default     = false
-}
-locals {
-  append_region_suffix_a = var.append_region_suffix_a != null ? var.append_region_suffix_a : false
+  nullable    = false
 }
 variable "append_region_suffix_b" {
   description = "If `true`, a suffix in the form of `-{region_name}` will be appended to the name of bucket B. This is convenient if you're creating buckets in multiple regions and don't want to manually specify the region name in each one for uniqueness. If not provided, will default to the value of `append_region_suffix_a`."
@@ -250,16 +208,14 @@ variable "append_region_suffix_b" {
   default     = null
 }
 locals {
-  append_region_suffix_b = var.append_region_suffix_b != null ? var.append_region_suffix_b : local.append_region_suffix_a
+  var_append_region_suffix_b = var.append_region_suffix_b != null ? var.append_region_suffix_b : var.append_region_suffix_a
 }
 
 variable "object_lock_enabled_a" {
   description = "Whether to enable Object Lock for bucket A."
   type        = bool
   default     = false
-}
-locals {
-  object_lock_enabled_a = var.object_lock_enabled_a != null ? var.object_lock_enabled_a : false
+  nullable    = false
 }
 variable "object_lock_enabled_b" {
   description = "Whether to enable Object Lock for bucket B. If not provided, will default to the value of `object_lock_enabled_a`."
@@ -267,16 +223,14 @@ variable "object_lock_enabled_b" {
   default     = false
 }
 locals {
-  object_lock_enabled_b = var.object_lock_enabled_b != null ? var.object_lock_enabled_b : local.object_lock_enabled_a
+  var_object_lock_enabled_b = var.object_lock_enabled_b != null ? var.object_lock_enabled_b : var.object_lock_enabled_a
 }
 
 variable "force_destroy_a" {
   description = "Whether all objects (including any locked objects) should be deleted from bucket A so that the bucket can be destroyed without error."
   type        = bool
   default     = false
-}
-locals {
-  force_destroy_a = var.force_destroy_a != null ? var.force_destroy_a : false
+  nullable    = false
 }
 variable "force_destroy_b" {
   description = "Whether all objects (including any locked objects) should be deleted from bucket B so that the bucket can be destroyed without error. If not provided, will default to the value of `force_destroy_a`."
@@ -284,16 +238,14 @@ variable "force_destroy_b" {
   default     = null
 }
 locals {
-  force_destroy_b = var.force_destroy_b != null ? var.force_destroy_b : local.force_destroy_a
+  var_force_destroy_b = var.force_destroy_b != null ? var.force_destroy_b : var.force_destroy_a
 }
 
 variable "force_allow_cloudtrail_digest_a" {
   description = "Whether to allow AES256 (AWS-managed key) encryption for paths checked by CloudTrail digest writers for bucket A. Even when a bucket and a CloudTrail are both set to use KMS encryption, digests are still written using AWS-managed key AES256 encryption (). This variable only has an effect when the `kms_key_id` variable is provided and not `null`."
   type        = bool
   default     = false
-}
-locals {
-  force_allow_cloudtrail_digest_a = var.force_allow_cloudtrail_digest_a != null ? var.force_allow_cloudtrail_digest_a : false
+  nullable    = false
 }
 variable "force_allow_cloudtrail_digest_b" {
   description = "Whether to allow AES256 (AWS-managed key) encryption for paths checked by CloudTrail digest writers for bucket B. Even when a bucket and a CloudTrail are both set to use KMS encryption, digests are still written using AWS-managed key AES256 encryption (). This variable only has an effect when the `kms_key_id` variable is provided and not `null`. If not provided, will default to the value of `force_allow_cloudtrail_digest_a`."
@@ -301,25 +253,19 @@ variable "force_allow_cloudtrail_digest_b" {
   default     = null
 }
 locals {
-  force_allow_cloudtrail_digest_b = var.force_allow_cloudtrail_digest_b != null ? var.force_allow_cloudtrail_digest_b : local.force_allow_cloudtrail_digest_a
+  var_force_allow_cloudtrail_digest_b = var.force_allow_cloudtrail_digest_b != null ? var.force_allow_cloudtrail_digest_b : var.force_allow_cloudtrail_digest_a
 }
 
 variable "replicate_a_to_b" {
   description = "Whether to replicate objects from bucket A to bucket B."
   type        = bool
-  validation {
-    condition     = var.replicate_a_to_b != null
-    error_message = "`replicate_a_to_b` may not be `null`."
-  }
+  nullable    = false
 }
 
 variable "replicate_b_to_a" {
   description = "Whether to replicate objects from bucket B to bucket A."
   type        = bool
-  validation {
-    condition     = var.replicate_b_to_a != null
-    error_message = "`replicate_b_to_a` may not be `null`."
-  }
+  nullable    = false
 }
 
 variable "a_to_b_rules" {
@@ -335,7 +281,8 @@ variable "a_to_b_rules" {
     event_threshold             = number
     storage_class               = string
   }))
-  default = []
+  default  = []
+  nullable = false
 }
 
 variable "b_to_a_rules" {
@@ -351,10 +298,6 @@ variable "b_to_a_rules" {
     event_threshold             = number
     storage_class               = string
   }))
-  default = []
-}
-
-locals {
-  a_to_b_rules = var.a_to_b_rules != null ? var.a_to_b_rules : []
-  b_to_a_rules = var.b_to_a_rules != null ? var.b_to_a_rules : []
+  default  = []
+  nullable = false
 }
